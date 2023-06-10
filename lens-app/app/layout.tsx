2f4b2080a26e77960e1afaf492423a74f4feecfa
wallclimbr <wallclimbr@mailbox.org>
2023-06-10 12:20:55 +0200

    merge

--- lens-app/app/layout.tsx
+++ lens-app/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 import { configureChains, createClient, WagmiConfig } from 'wagmi'
 import { mainnet, polygon } from 'wagmi/chains'
 import { publicProvider } from 'wagmi/providers/public'
-import { LensProvider, LensConfig, production } from '@lens-protocol/react-web'
+import { LensProvider, LensConfig, development } from '@lens-protocol/react-web'
 import { bindings as wagmiBindings } from '@lens-protocol/wagmi'
 const { provider, webSocketProvider } = configureChains([polygon, mainnet], [publicProvider()])
 
@@ -15,7 +15,7 @@ const client = createClient({
 
 const lensConfig: LensConfig = {
   bindings: wagmiBindings(),
-  environment: production,
+  environment: development,
 };
 
 export default function RootLayout({
